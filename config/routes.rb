6481a438bb8288960c14b909bafcd4ef1e878278
Marc-Antoine de More <marcantoine.demorel@gmail.com>
2018-06-28 16:42:30 +0200

    try to erase devise everywhere"

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'photographie', to: 'pages#photographie', as: :photographie
   get 'projets', to: 'pages#projets', as: :projets
   get 'contact', to: 'pages#contact', as: :contact
-  devise_for :users
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
